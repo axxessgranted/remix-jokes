b80d3a6766906eadd2b091c883e08bc2eca3c64e
Grant Kantor <58207637+axxessgranted@users.noreply.github.com>
2023-02-05 15:52:47 +0200

    added a style link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,2 +1,9 @@
+import type { LinksFunction } from '@remix-run/node';
+import stylesUrl from '~/styles/index.css';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesUrl },
+];
+
 const IndexRoute = () => <h1>Hello index Route</h1>;
 export default IndexRoute;
